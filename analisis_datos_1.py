32bfd263a5a6823517ad1c2b14b1eee1de7975f3
Sebas19812002 <js.vargasm1@uniandes.edu.co>
2023-03-12 14:42:47 -0500

    Eliminar caracteres que no sirven

--- analisis_datos_1.py
+++ analisis_datos_1.py
@@ -1,7 +1,15 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sun Mar 12 13:13:14 2023
+import matplotlib.pyplot as plt
+import pandas as pd
 
-@author: pc
-"""
+datos= pd.read_csv("Datos.data")
+datos.head()
 
+for i in range (0,len(datos)):
+    if datos["Ca"][i]=="?":
+        datos.drop(i, axis=0, inplace=True)
+    elif datos["Thal"][i]=="?":
+        datos.drop(i, axis=0, inplace=True)
+
+datos[["Ca","Thal"]]=datos[["Ca","Thal"]].apply(pd.to_numeric)
+
+scores=["Age","Trestbps", "Chol","Thalach","Oldpeak","Ca","Num"]
